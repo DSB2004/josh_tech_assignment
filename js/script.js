1a41e24d551b87b2e5f90bceae594ade0741070b
Damanjeet Singh <damanjeetsingh434@gmail.com>
2025-09-19 20:14:31 +0530

    temp push
    Signed-off-by: Damanjeet Singh <damanjeetsingh434@gmail.com>

--- js/script.js
+++ js/script.js
@@ -8,20 +8,17 @@ function createGridCard({
   return `
     <div class="w-fit flex flex-col gap-2" style="background-color: var(--white)">
       <img src="${imageSrc}" alt="${title}" width="300" height="300" />
-
       <div class="mx-4 flex flex-col gap-2 pb-2">
         <div class="flex items-center justify-between">
           <h4 class="card-h4">${title}</h4>
           <h4 class="card-h4">${price}</h4>
         </div>
-
         <div class="flex justify-between">
           <div class="flex justify-between items-center gap-4">
             <span class="flex justify-between items-center" style="background-color: var(--background-white);">
               <img src="./assets/icons/star.svg" alt="">
               <span class="card-span">${rating}</span>
             </span>
-
             <span class="card-span" style="background-color: var(--background-white);">
               [ ${time} ]
             </span>
@@ -33,30 +30,81 @@ function createGridCard({
   `;
 }
 
-function init() {
-  const parent = document.getElementById("kitchen-card-grid");
-  fetch("./assets/static/products.json").then((res) => {
-    res.json().then((value) => {
-      let cards = "";
-      for (let val of value) {
-        cards += createGridCard(val);
-      }
-      parent.innerHTML = cards;
-    });
-  });
+function createSlideCard({
+  imageSrc = "./assets/images/product.png",
+  title = "Product Title",
+  price = "₹0",
+  rating = "0.0",
+  time = "0-0 min",
+}) {
+  return `
+    <div class="swiper-slide">
+      <div class="w-fit m-auto shadow-md flex flex-col gap-2 rounded-md" style="background-color: var(--white)">
+        <img src="${imageSrc}" alt="${title}" width="300" height="300" />
+        <div class="mx-4 flex flex-col gap-2 pb-2">
+          <div class="flex items-center justify-between">
+            <h4 class="card-h4">${title}</h4>
+            <h4 class="card-h4">${price}</h4>
+          </div>
+          <div class="flex justify-between">
+            <div class="flex justify-between items-center gap-4">
+              <span class="flex justify-between items-center" style="background-color: var(--background-white);">
+                <img src="./assets/icons/star.svg" alt="">
+                <span class="card-span">${rating}</span>
+              </span>
+              <span class="card-span" style="background-color: var(--background-white);">
+                [ ${time} ]
+              </span>
+            </div>
+            <img src="./assets/icons/plus.svg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  `;
 }
 
-init();
+function init() {
+  const grid = document.getElementById("kitchen-card-grid");
+  const carousel = document.getElementById("carousel");
 
-const videoWrapper = document.getElementById("video-box");
-const videoButton = document.getElementById("video-button");
-const video = document.getElementById("video");
-video.addEventListener("click", () => {
-  if (video.paused) {
-    videoButton.style.opacity = "0%";
-    video.play();
-  } else {
-    videoButton.style.opacity = "100%";
-    video.pause();
-  }
-});
+  fetch("./assets/static/products.json")
+    .then((res) => res.json())
+    .then((value) => {
+      let cards = "";
+      let slide = "";
+      for (let val of value) {
+        cards += createGridCard(val);
+        slide += createSlideCard(val);
+      }
+      grid.innerHTML = cards;
+      carousel.innerHTML = slide;
+
+      new Swiper(".swiper", {
+        direction: "horizontal",
+        loop: false,
+        slidesPerView: 3,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    });
+}
+
+// Run everything after page load
+window.onload = () => {
+  init();
+  const videoButton = document.getElementById("video-button");
+  const video = document.getElementById("video");
+
+  video.addEventListener("click", () => {
+    if (video.paused) {
+      videoButton.style.opacity = "0%";
+      video.play();
+    } else {
+      videoButton.style.opacity = "100%";
+      video.pause();
+    }
+  });
+};
